eeb2ede6913691a9fc15f94e2b9e93ac3b0abeed
ramtarun <balagam.r@husky.neu.edu>
2020-08-01 20:33:12 -0400

    Schema changes for Return Booking

--- models/Bookings.js
+++ models/Bookings.js
@@ -7,47 +7,51 @@ let BookingSchema = new Schema({
     type: Date,
     default: Date.now,
   },
-  journeyDate: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
   numPassengers: {
     type: Number,
     required: true,
     default: 0,
   },
   passengerDetails: [PassengerSchema],
-  flightNo: {
-    type: String,
-    required: true,
-    default: 00000,
-  },
-  airlineName: {
-    type: String,
-    required: true,
-    default: "ABC Airlines",
+  onwardFlightDetails: {
+    journeyDate: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    flightNo: {
+      type: String,
+      required: true,
+      default: 00000,
+    },
+    airlineName: {
+      type: String,
+      required: true,
+      default: 'ABC Airlines',
+    },
   },
   returnFlag: {
     type: Boolean,
     required: true,
-    default: false
+    default: false,
   },
-  returnFlightNo: {
-    type: String,
-    required: false,
-    default: 00000,
+  returnFlightDetails: {
+    flightNo: {
+      type: String,
+      required: false,
+      default: 00000,
+    },
+    airlineName: {
+      type: String,
+      required: false,
+      default: '00000000',
+    },
+    journeyDate: {
+      type: Date,
+      required: false,
+      default: Date.now,
+    },
   },
-  returnAirlineName: {
-    type: String,
-    required: false,
-    default: "ABC Airlines",
-  },
-  returnJourneyDate: {
-    type: Date,
-    required: false,
-    default: Date.now,
-  }
 });
 
 module.exports = Bookings = mongoose.model('bookinglist', BookingSchema, 'bookinglist');
