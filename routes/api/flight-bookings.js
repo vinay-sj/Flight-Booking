850155b77b352e4a48c0dcc41114fc10a454ce3c
Dhaval <dhavalmohandas@gmail.com>
2020-08-04 19:03:42 -0400

    Add bookings delete api

--- routes/api/flight-bookings.js
+++ routes/api/flight-bookings.js
@@ -7,7 +7,7 @@ const { OneWayBookings, RoundWayBookings } = require('../../models/Bookings');
 
 // List of Flights and prices directly from UI layer to Skyscanner API
 
-// Confirmed Bookings API
+// Get Confirmed Bookings
 router.get('/oneWayBookings', (req, res) => {
   OneWayBookings.find()
   .sort({ bookingDate: -1 })
@@ -19,8 +19,6 @@ router.get('/roundTripBookings', (req, res) => {
   .sort({ bookingDate: -1 })
   .then((items) => res.json(items));
 });
-
-
 
 // Create a new Booking
 router.post('/confirmBooking', (req, res) => {
@@ -35,9 +33,18 @@ router.post('/confirmBooking', (req, res) => {
   confirmBooking.save().then((item) => res.json(item), (err) => res.json(err));
 });
 
-// Delete Booking
-router.delete('/deleteOneWay:id',(req, res) => {
-  
-})
+// Delete One Way Booking
+router.delete('/deleteOneWayTrip/:id', (req,res) => {
+  OneWayBookings.findById(req.params.id)
+    .then(booking => booking.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false}));
+});
+
+// Delete Round Trip Booking
+router.delete('/deleteRoundTrip/:id', (req,res) => {
+  RoundWayBookings.findById(req.params.id)
+  .then(booking => booking.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false}));
+});
 
 module.exports = router;
