43a65d1eb0fcb3978923893cd82aa2d403938b03
ramtarun <balagam.r@husky.neu.edu>
2020-08-06 23:54:34 -0400

    API Authorization using JWT verification

--- routes/api/flight-bookings.js
+++ routes/api/flight-bookings.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const { OneWayBookings, RoundWayBookings } = require('../../models/Bookings');
+const { validateAPIRequest } = require('../../auth.js');
+
+let userCredentials = {};
+
+// Default Validation of all API requests towards Bookings 
+router.use('/', (req, res, next) => {
+  userCredentials = validateAPIRequest(req, res) || {};
+  console.log(userCredentials);
+  next()
+});
 
 // List of Cities and Countries directly from UI layer to Skyscanner API
 
@@ -9,42 +19,56 @@ const { OneWayBookings, RoundWayBookings } = require('../../models/Bookings');
 
 // Get Confirmed Bookings
 router.get('/oneWayBookings', (req, res) => {
-  OneWayBookings.find()
-  .sort({ bookingDate: -1 })
-  .then((items) => res.json(items));
+  if (userCredentials.signedIn) {
+    OneWayBookings.find({ userEmail: userCredentials.email })
+      .sort({ bookingDate: -1 })
+      .then((items) => res.json(items));
+  }
 });
 
 router.get('/roundTripBookings', (req, res) => {
-  RoundWayBookings.find()
-  .sort({ bookingDate: -1 })
-  .then((items) => res.json(items));
+  if (userCredentials.signedIn) {
+    RoundWayBookings.find({ userEmail: userCredentials.email })
+      .sort({ bookingDate: -1 })
+      .then((items) => res.json(items));
+  }
 });
 
 // Create a new Booking
-router.post('/confirmBooking', (req, res) => {
-  const confirmBooking = !req.body.isRoundTrip
-    ? new OneWayBookings({
-        ...req.body,
-      })
-    : new RoundWayBookings({
-        ...req.body,
-      });
+router.post('/confirmBooking', async (req, res) => {
+  if (userCredentials.signedIn) {
+    const confirmBooking = !req.body.isRoundTrip
+      ? new OneWayBookings({
+          ...req.body,
+        })
+      : new RoundWayBookings({
+          ...req.body,
+        });
 
-  confirmBooking.save().then((item) => res.json(item), (err) => res.json(err));
+    confirmBooking.save().then(
+      (item) => res.json(item),
+      (err) => res.status(400).json(err)
+    );
+  }
 });
 
 // Delete One Way Booking
-router.delete('/deleteOneWayTrip/:id', (req,res) => {
-  OneWayBookings.findById(req.params.id)
-    .then(booking => booking.remove().then(() => res.json({ success: true })))
-        .catch(err => res.status(404).json({ success: false}));
+router.delete('/deleteOneWayTrip/:id', (req, res) => {
+  console.log("reached here")
+  if (userCredentials.signedIn) {
+    OneWayBookings.findById(req.params.id)
+      .then((booking) => booking.remove().then(() => res.json({ success: true })))
+      .catch((err) => res.status(404).json({ success: false }));
+  }
 });
 
 // Delete Round Trip Booking
-router.delete('/deleteRoundTrip/:id', (req,res) => {
-  RoundWayBookings.findById(req.params.id)
-  .then(booking => booking.remove().then(() => res.json({ success: true })))
-        .catch(err => res.status(404).json({ success: false}));
+router.delete('/deleteRoundTrip/:id', (req, res) => {
+  if (userCredentials.signedIn) {
+    RoundWayBookings.findById(req.params.id)
+      .then((booking) => booking.remove().then(() => res.json({ success: true })))
+      .catch((err) => res.status(404).json({ success: false }));
+  }
 });
 
 module.exports = router;
