223e8c1ad432925fb4a2b247e57c32c074ca5fd7
ramtarun <balagam.r@husky.neu.edu>
2020-08-12 13:06:49 -0400

    push to personal website

--- auth.js
+++ auth.js
@@ -38,9 +38,7 @@ routes.post('/signin', async (req, res) => {
   console.log('COOKie_DOMAIN')
   console.log(process.env.COOKIE_DOMAIN)
   res.cookie('jwt', token, { httpOnly: true, sameSite: 'None', secure: true });
-    // , path: '/', sameSite:false, domain: process.env.COOKIE_DOMAIN });
   console.log(res)
-  // domain: process.env.COOKIE_DOMAIN,
   res.json(credentials);
 });
 
