025cf5b799f7f2a0a90d15e68970c926e2c7c362
Vinay Srampickal Joseph <srampickaljoseph.v@husky.neu.edu>
2020-08-12 09:52:01 -0400

    commented cookie domain

--- auth.js
+++ auth.js
@@ -37,7 +37,8 @@ routes.post('/signin', async (req, res) => {
   console.log(token)
   console.log('COOKie_DOMAIN')
   console.log(process.env.COOKIE_DOMAIN)
-  res.cookie('jwt', token, { httpOnly: true, path: '/', sameSite:false, domain: process.env.COOKIE_DOMAIN });
+  res.cookie('jwt', token, { httpOnly: true });
+    // , path: '/', sameSite:false, domain: process.env.COOKIE_DOMAIN });
   console.log(res)
   res.json(credentials);
 });
